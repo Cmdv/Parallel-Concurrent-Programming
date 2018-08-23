4ae0d0e6d7ffa72faf97fb9ec374ac8bda44c0ad
Cmdv <vincent@cmdv.me>
2018-08-23 21:28:24 +0100

    Chapter 2 and strat.hs with Chapter 3

---
+++ src/Chapter02/Sudoku/Sudoku5.hs
@@ -0,0 +1,17 @@
+module Main where
+
+import Control.Exception
+import Control.Parallel.Strategies
+import Data.Maybe
+import Sudoku
+import System.Environment
+
+main :: IO ()
+main = do
+  [f] <- getArgs
+  file <- readFile f
+
+  let puzzles   = lines file
+      solutions = map solve puzzles `using` parList rseq
+
+  print (length (filter isJust solutions))
